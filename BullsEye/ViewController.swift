69e9d7b1e6955431cd70ee66bd74b02ad0ab96bc
Danyal Aboobakar <danyal.aboobakar@gmail.com>
2017-01-19 18:02:29 +0000

    Add: Round Functionality
    BullsEye now has rounds.

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,35 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
     var currentValue: Int = 50
+    var targetValue: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        currentValue = lroundf(slider.value)
+        startNewRound()
+        updateLabels()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
 
     @IBAction func showAlert() {
-        let message = "The value of the slider is: \(currentValue)"
+        let message = "The value of the slider is: \(currentValue)" +
+        "\nThe target value is: \(targetValue)"
         
         let alert = UIAlertController(title: "Slider value", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
-    
+        
+        startNewRound()
+        updateLabels()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
     
         currentValue = lroundf(slider.value)
     
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
     }
 
 }
