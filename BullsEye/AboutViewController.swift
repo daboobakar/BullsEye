5cf7f73b9774cd7bc6d9130bc8a691d61e785ef4
Danyal Aboobakar <danyal.aboobakar@gmail.com>
2017-01-23 14:56:38 +0000

    All functionality complete Add: About screen
    - All of BullsEye’s functionality have been completed.
    
    - An about screen has been added to tell the user how the game works.

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Danyal Aboobakar on 23/01/2017.
+//  Copyright © 2017 Danyal Aboobakar. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func closeView(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+
+}
